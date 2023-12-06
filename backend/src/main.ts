084fdcc26650d56e534e798642d6c5f474e9d394
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-12-06 14:16:40 +0300

    feat: добавлен helmet и env шаблон

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { JWTAuthGuard } from './common/guards/jwt.guard';
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JWTAuthGuard(reflector));
