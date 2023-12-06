b434efc1f9b47ad4927ba668d58348a1ce54f56c
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-12-06 16:49:27 +0300

    feat: docker-compose для всей связки

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,11 +2,11 @@ import * as process from 'process';
 
 export default () => ({
   db: {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || '5432',
-    name: process.env.DB_NAME || 'kupipodariday',
-    user: process.env.DB_USER || 'student',
-    password: process.env.DB_PASSWORD || 'student',
+    host: process.env.POSTGRES_HOST || 'localhost',
+    port: process.env.POSTGRES_PORT || '5432',
+    name: process.env.POSTGRES_DB || 'kupipodari',
+    user: process.env.POSTGRES_USER || 'student',
+    password: process.env.POSTGRES_PASSWORD || 'student',
   },
   jwt: {
     secret: process.env.JWT_SECRET,
